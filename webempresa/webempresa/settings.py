17928eb2194077c1f141ebfae599619cb5e1fc69
Ronald Vélez <75101671+roxanderve@users.noreply.github.com>
2021-02-02 10:51:37 -0500

    Update settings.py

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w_kj@d_bf-3d5!b@j93+j$g$6rrqpm&aew5855vlqgf133=igm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roxanderve.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
